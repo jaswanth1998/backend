4008313b48b14bad3e267b2f453c59dadada258f
jaswanth tata <jaswanthtata@gmail.com>
2019-02-02 09:54:05 +0530

    first commit

---
+++ server/boot/root.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function(server) {
+  // Install a `/` route that returns server status
+  var router = server.loopback.Router();
+  router.get('/', server.loopback.status());
+  server.use(router);
+};
+module.exports = function(MyModel) {
+  // send an email
+  MyModel.sendEmail = function(cb) {
+    MyModel.app.models.Email.send({
+      to: 'jaswanthtata@gmail.com',
+      from: 'you@gmail.com',
+      subject: 'my subject',
+      text: 'my text',
+      html: 'my <em>html</em>'
+    }, function(err, mail) {
+      console.log('email sent!');
+      cb(err);
+    });
+  }
+};
---
+++ server/server.js
@@ -0,0 +1,48 @@
+'use strict';
+
+var loopback = require('loopback');
+var boot = require('loopback-boot');
+var loopbackPassport = require('loopback-component-passport');
+var PassportConfigurator = loopbackPassport.PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
+
+
+
+
+var config = {};
+try {
+	config = require('../providers.json');
+	// If using custom passport module
+	// config['custom-example'].verifyMethod = function(req, token, details, verified) {
+	// 	verified(null, details);
+	// }
+} catch (err) {
+	console.trace(err);
+	process.exit(1); // fatal
+}
+
+var app = module.exports = loopback();
+
+app.start = function() {
+  // start the web server
+  return app.listen(function() {
+    app.emit('started');
+    var baseUrl = app.get('url').replace(/\/$/, '');
+    console.log('Web server listening at: %s', baseUrl);
+    if (app.get('loopback-component-explorer')) {
+      var explorerPath = app.get('loopback-component-explorer').mountPath;
+      console.log('Browse your REST API at %s%s', baseUrl, explorerPath);
+    }
+  });
+};
+
+// Bootstrap the application, configure models, datasources and middleware.
+// Sub-apps like REST API are mounted via boot scripts.
+boot(app, __dirname, function(err) {
+  if (err) throw err;
+
+  // start the server if `$ node server.js`
+  if (require.main === module)
+    app.start();
+});
